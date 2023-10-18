189ef7b936147f69bf5e4fa45c23afa4114a4818
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2023-10-18 23:07:46 +0545

    Error Handling completed

--- axios/src/App.jsx
+++ axios/src/App.jsx
@@ -6,15 +6,18 @@ import axios from "axios";
 
 function App() {
   const [data, setData] = useState([]);
+  const [isError, setIsError] = useState("");
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => setData(res.data));
+      .get("https://jsonplaceholder.typicode.com/poss")
+      .then((res) => setData(res.data))
+      .catch((error) => setIsError(error.message));
   }, []);
 
   return (
     <>
       <h1>My data</h1>
+      {isError !== "" && <h3>{isError}</h3>}
       <ol>
         {data.map((item) => (
           <li key={item.id}>{item.title}</li>
