8c0880d6418d52d71d3fd02a05ffbead6008d656
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2023-10-18 23:15:34 +0545

    Used async await

--- axios/src/App.jsx
+++ axios/src/App.jsx
@@ -3,16 +3,33 @@ import "./App.css";
 import axios from "axios";
 
 // NOTE: Using Promises
+// const [data, setData] = useState([]);
+// const [isError, setIsError] = useState("");
+// useEffect(() => {
+//   axios
+//     .get("https://jsonplaceholder.typicode.com/poss")
+//     .then((res) => setData(res.data))
+//     .catch((error) => setIsError(error.message));
+// }, []);
+
+// NOTE: Using Async
 
 function App() {
   const [data, setData] = useState([]);
   const [isError, setIsError] = useState("");
+
   useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/poss")
-      .then((res) => setData(res.data))
-      .catch((error) => setIsError(error.message));
+    getApiData();
   }, []);
+
+  const getApiData = async () => {
+    try {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      setData(res.data);
+    } catch (error) {
+      setIsError(error.message);
+    }
+  };
 
   return (
     <>
