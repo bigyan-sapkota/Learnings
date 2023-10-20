ac45468295bb31304754b1d6b5478be964227880
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2023-10-20 16:35:36 +0545

    Used axios to fetch data

--- react-sheriyans/app/page.js
+++ react-sheriyans/app/page.js
@@ -1,9 +1,34 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import axios from "axios";
 
 const page = () => {
+  const [Images, setImages] = useState([]);
+
+  const getImages = async () => {
+    try {
+      const response = await axios.get("https://picsum.photos/v2/list");
+      const myData = response.data;
+      setImages(myData);
+    } catch (error) {
+      console.log("Error Fetching Images");
+    }
+  };
+
   return (
     <div>
-      <h1 className="text-4xl font-bold">This is Home Page</h1>
+      <button
+        onClick={getImages}
+        className="mt-4 ml-4 px-5 py-3 bg-green-600 text-white rounded"
+      >
+        Get Images
+      </button>
+
+      <div>
+        {Images.map((img, i) => (
+          <div key={i}>{img.author}</div>
+        ))}
+      </div>
     </div>
   );
 };
