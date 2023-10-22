4bddd267174c7dce686f0551ce7f7c8a197576ca
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2023-10-22 16:08:57 +0545

    Learnt to use npm packages

--- nodeJS/script.js
+++ nodeJS/script.js
@@ -1,3 +1,20 @@
-var a = 12;
+// NPM contain many packages which can be used by us to save our time.
+// nmp i <packageName> can be used to install a package.
 
-module.exports = a;
+// ONE LINER JOKE:
+// var oneLinerJoke = require("one-liner-joke");
+// console.log(oneLinerJoke.getRandomJoke());
+
+// FIGLET:
+// var figlet = require("figlet");
+// figlet("B I G YA N", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+// var a = 12;
+// module.exports = a;
