dc9da6b22452847a8b0127f280609fee1e05363a
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2023-11-11 20:58:38 +0545

    Initializtion

---
+++ dsa with js/Singly Linked List/1_signlyLinkedList.js
@@ -0,0 +1,27 @@
+// piece of data - val
+// referencew to next node - next
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
+
+// var first = new Node("Hi");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you?");
+
+var list = new SinglyLinkedList();
+list.push("Hello");
+list.push("Goodbye");
