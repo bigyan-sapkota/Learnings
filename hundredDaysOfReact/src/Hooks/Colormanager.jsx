172790dbbbe40a87d826f9c3bbe8fc893d852236
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2023-11-15 22:47:43 +0545

    Sucessfully made the color palette <3

--- hundredDaysOfReact/src/Hooks/Colormanager.jsx
+++ hundredDaysOfReact/src/Hooks/Colormanager.jsx
@@ -2,18 +2,33 @@ import React, { useState } from "react";
 
 const Colormanager = () => {
   const [colors, setColors] = useState(["#111111", "#ffffff", "#f40d0d"]);
+  const [newColor, setNewColor] = useState("");
+  const [error, setError] = useState("");
 
-  const addColor = () => {};
+  const addColor = (newColor) => {
+    const colorExists = colors.includes(newColor);
 
-  const removeColor = () => {};
+    if (colorExists) {
+      setError("Color already used");
+    } else {
+      setColors([...colors, newColor]);
+    }
+  };
+
+  const removeColor = (i) => {
+    const updatedColor = [...colors];
+    updatedColor.splice(i, 1);
+    setColors(updatedColor);
+  };
 
   return (
     <div className="m-10">
+      <div className="font-extrabold text-4xl mb-7">Colors List:</div>
       <div className="flex flex-col gap-4">
         {colors.map((color, i) => (
           <div
             key={i}
-            className="flex align-center justify-between p-4 border border-gray-400 rounded-md w-1/5 shadow-lg"
+            className="flex align-center justify-between p-4 border border-gray-400 rounded-md w-1/4 shadow-lg"
           >
             <div
               className="h-5 w-5 border-2 border-black"
@@ -22,8 +37,37 @@ const Colormanager = () => {
               }}
             ></div>
             <div className="font-bold">{color}</div>
+            <button
+              className="bg-red-600 px-3 py-1 text-white font-normal rounded-md"
+              onClick={() => removeColor(i)}
+            >
+              Delete
+            </button>
           </div>
         ))}
+      </div>
+
+      <div className="font-extrabold text-4xl my-7">Add Colors:</div>
+      <div>
+        <input
+          type="text"
+          className="border border-black mb-4 w-1/4 pl-2 py-2"
+          value={newColor}
+          onChange={(e) => {
+            setNewColor(e.target.value);
+          }}
+        />
+        <div className="text-[#ff0000] font-bold flex gap-1">
+          <div className="underline">Note:</div>
+          {error}
+        </div>
+        <br />
+        <button
+          className="bg-green-600 px-3 py-1 text-white font-normal rounded-md"
+          onClick={() => addColor(newColor)}
+        >
+          Add
+        </button>
       </div>
     </div>
   );
