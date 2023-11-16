2bde23a2e41ece470c1b5a8ec03957de583978e7
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2023-11-16 22:26:46 +0545

    Gettomg Started with dropzone

--- hundredDaysOfReact/src/Components/Main.jsx
+++ hundredDaysOfReact/src/Components/Main.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import Colormanager from "../Hooks/Colormanager";
 
 const Main = () => {
-  return (
-    <div>
-      <Colormanager />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Main;
