10a0b42a4f10b06d5fc90d2ae76cb153a64fdbc8
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2023-11-19 23:06:43 +0545

    Added filter function successfully

--- hundredDaysOfReact/src/Hooks/TaskScheduler.jsx
+++ hundredDaysOfReact/src/Hooks/TaskScheduler.jsx
@@ -11,6 +11,21 @@ const TaskScheduler = () => {
     },
     { task: "Run 6 miles", category: "Workout", dueDate: "2023/12/23" },
   ]);
+
+  const [filteredTask, setFilteredTask] = useState(tasks);
+  const menuItems = ["All", "Study", "Workout", "Coding"];
+
+  const categoryHandler = (selectedCategory) => {
+    if (selectedCategory === "All") {
+      setFilteredTask(tasks);
+    } else {
+      const filtered = tasks.filter(
+        (task) => task.category === selectedCategory
+      );
+      setFilteredTask(filtered);
+    }
+  };
+
   return (
     <div>
       <div className="p-10">
@@ -19,16 +34,20 @@ const TaskScheduler = () => {
             Task <span className="text-purple-600">Scheduler</span>
           </h1>
           <nav className="flex mb-10">
-            {tasks.map((item, i) => (
-              <button className="px-2 py-1 border-2 border-neutral-300 bg-white-700 rounded-md mr-2 text-purple-600 font-medium border-b-4 border-b-purple-600 border-r-4 border-r-purple-600 shadow-sm hover:bg-purple-600 hover:text-white hover:border-purple-600">
-                {item.category}
+            {menuItems.map((item, i) => (
+              <button
+                key={i}
+                className="px-2 py-1 border-2 border-neutral-300 bg-white-700 rounded-md mr-2 text-purple-600 font-medium border-b-4 border-b-purple-600 border-r-4 border-r-purple-600 shadow-sm hover:bg-purple-600 hover:text-white hover:border-purple-600"
+                onClick={() => categoryHandler(item)}
+              >
+                {item}
               </button>
             ))}
           </nav>
         </div>
 
         <div className="flex flex-col items-center">
-          {tasks.map((item, i) => (
+          {filteredTask.map((item, i) => (
             <div
               key={i}
               className="p-7 shadow-md mb-8 border border-neutral-200 w-4/5"
