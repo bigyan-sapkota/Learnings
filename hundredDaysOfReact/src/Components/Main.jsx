bfe9f48f326c0cad4ec86cf557384344cf483300
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2023-11-30 11:10:50 +0545

    Completed course

--- hundredDaysOfReact/src/Components/Main.jsx
+++ hundredDaysOfReact/src/Components/Main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import CourseForm from "../Zustand/CourseForm";
+import CourseList from "../Zustand/CourseList";
 
 const Main = () => {
   return (
     <div>
+      <CourseList />
       <CourseForm />
     </div>
   );
--- hundredDaysOfReact/src/Zustand/CourseList.jsx
+++ hundredDaysOfReact/src/Zustand/CourseList.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import useCourseStore from "./courseStore";
 
 const CourseList = () => {
-  return <div>CourseList</div>;
+  const { courses, removeCourse, toggleCourseStatus } = useCourseStore(
+    (state) => ({
+      courses: state.courses,
+      removeCourse: state.removeCourse,
+      toggleCourseStatus: state.toggleCourseStatus,
+    })
+  );
+  return (
+    <>
+      <ul>
+        {courses.map((course, i) => {
+          return (
+            <React.Fragment key={i}>
+              <li
+                className={`course-item text-black`}
+                style={{
+                  backgroundColor: course.completed ? "#00ff0044" : "white",
+                }}
+              >
+                <span className="course-item-col-1">
+                  <input
+                    type="checkbox"
+                    checked={course.completed}
+                    onChange={(e) => {
+                      toggleCourseStatus(course.id);
+                    }}
+                  />
+                </span>
+                <span>{course?.title}</span>
+                <button
+                  className="delete btn"
+                  onClick={() => {
+                    removeCourse(course.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </li>
+            </React.Fragment>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default CourseList;
