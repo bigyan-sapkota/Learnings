336b15e7e14b222cd0081b56753da8c17582e735
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2023-12-06 07:40:11 +0545

    Functions

---
+++ php/02_functions.php
@@ -0,0 +1,23 @@
+<!-- Functions are the block of reusable code that perform specific task -->
+
+<html>
+    <head>
+        <title>Functions</title>
+    </head>
+    <body>
+        <?php
+            // FUNCTION WITHOUT PARAMETER:
+            // function greet() {
+            //     echo("Hello World!");
+            // }
+            // greet();
+
+            // FUNCTION WITH PARAMETER
+            // function addNumbers($a, $b) {
+            //     $sum = $a + $b;
+            //     echo("The sum of $a and $b is: $sum");
+            // }
+            // addNumbers(1,2);
+        ?>
+    </body>
+</html>
